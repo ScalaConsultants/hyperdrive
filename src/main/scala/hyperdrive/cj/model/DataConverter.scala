9f5cfe41d61cd4a95901102d37b4ebf05cb0b9ee
Paweł Śląski <slaskip@gmail.com>
2017-01-17 13:19:51 +0100

    separated TemplateConverter from DataConverter and added tests

--- src/main/scala/hyperdrive/cj/model/DataConverter.scala
+++ src/main/scala/hyperdrive/cj/model/DataConverter.scala
@@ -1,7 +1,6 @@
 package hyperdrive.cj.model
 
 import shapeless.labelled.FieldType
-import shapeless.tag.Tagged
 import shapeless.{::, HList, HNil, LabelledGeneric, Lazy, Witness}
 
 trait DataConverter[T] {
@@ -9,6 +8,8 @@ trait DataConverter[T] {
 }
 
 object DataConverter {
+
+  def apply[T](implicit dataConverter: DataConverter[T]): DataConverter[T] = dataConverter
 
   def makeConverter[T](converter: T => Seq[Data]): DataConverter[T] = (obj: T) => converter(obj)
 
@@ -25,27 +26,4 @@ object DataConverter {
                                          encoder: Lazy[DataConverter[Repr]]) =
     makeConverter[T](value => encoder.value.toData(generic.to(value)))
   
-}
-
-trait TemplateConverter[T] {
-  def toTemplate: Template
-}
-
-object TemplateConverter {
-  def makeConverter[T](converter: => Template): TemplateConverter[T] = new TemplateConverter[T] {
-    override def toTemplate = converter
-  }
-
-  implicit val hNilConverter = makeConverter[HNil](Template())
-  implicit def hConsConverter[K <: Symbol, V, L <: HList](implicit keyWitness: Witness.Aux[K],
-                                                          tailConverter: TemplateConverter[L]) =
-    makeConverter[FieldType[K, V] :: L] {
-      val label = keyWitness.value.name
-      Data(name = label, value = None) +: tailConverter.toTemplate
-    }
-
-  implicit def genericConverter[T, Repr <: HList, Left <: HList, Right <: HList](implicit generic: LabelledGeneric.Aux[T, Repr], 
-                                         partitionBySubType: PartitionBySubType.Aux[Repr, Tagged[Id], Left, Right],
-                                         encoder: Lazy[TemplateConverter[Right]]) =
-    makeConverter[T](encoder.value.toTemplate)
 }
