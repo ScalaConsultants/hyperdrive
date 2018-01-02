554d7e6fdfbdd52403e3eaaf66f1da2b49e1dbff
Marcin Wysokiński <marcinwysokinski@Marcins-MacBook-Pro.local>
2018-01-02 11:00:27 +0100

    bump up libs versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object BuildSettings {
 
-  val akkaHttpVersion = "10.0.10"
+  val akkaHttpVersion = "10.0.11"
 
 }
 
@@ -11,8 +11,8 @@ object Dependencies {
   val common = Seq(
     "com.typesafe.akka" %% "akka-http" % BuildSettings.akkaHttpVersion, 
     "com.typesafe.akka" %% "akka-http-spray-json" % BuildSettings.akkaHttpVersion,
-    "org.typelevel" %% "cats-core" % "1.0.0-RC1",
-    "com.chuusai" %% "shapeless" % "2.3.2",
+    "org.typelevel" %% "cats-core" % "1.0.1",
+    "com.chuusai" %% "shapeless" % "2.3.3",
     "org.scalatest" %% "scalatest" % "3.0.4" % "test"
   )
 
