8d1c3d05167e76cea2d44e523e8a1841006ed25a
jczuchnowski <jakub.czuchnowski@gmail.com>
2018-05-24 11:46:01 +0200

    upgrade libs versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object BuildSettings {
 
-  val akkaHttpVersion = "10.0.10"
+  val akkaHttpVersion = "10.1.0"
 
 }
 
@@ -11,9 +11,10 @@ object Dependencies {
   val common = Seq(
     "com.typesafe.akka" %% "akka-http" % BuildSettings.akkaHttpVersion, 
     "com.typesafe.akka" %% "akka-http-spray-json" % BuildSettings.akkaHttpVersion,
-    "org.typelevel" %% "cats-core" % "1.0.0-RC1",
-    "com.chuusai" %% "shapeless" % "2.3.2",
-    "org.scalatest" %% "scalatest" % "3.0.4" % "test"
+    "com.typesafe.akka" %% "akka-stream" % "2.5.12",
+    "org.typelevel" %% "cats-core" % "1.0.1",
+    "com.chuusai" %% "shapeless" % "2.3.3",
+    "org.scalatest" %% "scalatest" % "3.0.5" % "test"
   )
 
 }
